56f5fdd131f620bb73702dd3cf5fa447886a428b
[Nguyet12] <[tieumaomi19@kyonggi.ac.kr]>
2024-07-30 12:22:08 +0900

    Record 다시 구현

--- src/main/java/com/dmc/bootcamp/repository/RecordRepository.java
+++ src/main/java/com/dmc/bootcamp/repository/RecordRepository.java
@@ -2,7 +2,6 @@ package com.dmc.bootcamp.repository;
 
 import com.dmc.bootcamp.domain.Record;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
 
 public interface RecordRepository extends JpaRepository<Record,Long> {
 
